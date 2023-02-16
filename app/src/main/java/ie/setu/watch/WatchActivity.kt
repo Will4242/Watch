57e498b1b78b577c494c7469ea94a77d84732a6e
Will <20088880@mail.wit.ie>
2023-02-16 10:31:45 +0000

    watch activity

--- app/src/main/java/ie/setu/watch/WatchActivity.kt
+++ app/src/main/java/ie/setu/watch/WatchActivity.kt
@@ -2,19 +2,41 @@ package ie.setu.watch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import ie.setu.watch.databinding.ActivityWatchBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class WatchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWatchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_watch)
+
+        binding = ActivityWatchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
 
-        i("Placemark Activity started..")
+        i("Watch Activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            val watchTitle = binding.watchTitle.text.toString()
+            if (watchTitle.isNotEmpty()) {
+                i("add Button Pressed: $watchTitle")
+            } else {
+                Snackbar
+                    .make(it, "Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
+
+
+
+
+
 
 
 
