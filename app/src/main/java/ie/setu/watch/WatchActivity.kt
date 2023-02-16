6ac5a8cf2b7d55bd07b0d67fb87a4618127037e3
Will <20088880@mail.wit.ie>
2023-02-16 08:56:22 +0000

    Renamed from mainActivity to WatchActivity

--- app/src/main/java/ie/setu/watch/WatchActivity.kt
+++ app/src/main/java/ie/setu/watch/WatchActivity.kt
@@ -3,7 +3,7 @@ package ie.setu.watch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WatchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
