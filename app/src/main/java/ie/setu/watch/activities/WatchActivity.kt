c8c202fc67a4dc7ca1dcd76cac530af496237310
Will <20088880@mail.wit.ie>
2023-02-16 10:54:03 +0000

    making use of model im watch activity

--- app/src/main/java/ie/setu/watch/activities/WatchActivity.kt
+++ app/src/main/java/ie/setu/watch/activities/WatchActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import ie.setu.watch.databinding.ActivityWatchBinding
+import ie.setu.watch.models.WatchModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class WatchActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWatchBinding
+    var watch = WatchModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,17 +24,19 @@ class WatchActivity : AppCompatActivity() {
         i("Watch Activity started...")
 
         binding.btnAdd.setOnClickListener() {
-            val watchTitle = binding.watchTitle.text.toString()
-            if (watchTitle.isNotEmpty()) {
-                i("add Button Pressed: $watchTitle")
-            } else {
+            watch.title = binding.watchTitle.text.toString()
+            if (watch.title.isNotEmpty()) {
+                i("add Button Pressed: $watch.title")
+            }
+            else {
                 Snackbar
-                    .make(it, "Please Enter a title", Snackbar.LENGTH_LONG)
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
     }
 }
+
 
 
 
