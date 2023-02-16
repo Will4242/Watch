64b122d6d6fe963c9ec1c6d14200da79a799016c
Will <20088880@mail.wit.ie>
2023-02-16 09:35:29 +0000

    timber in watchactivity

--- app/src/main/java/ie/setu/watch/WatchActivity.kt
+++ app/src/main/java/ie/setu/watch/WatchActivity.kt
@@ -2,11 +2,16 @@ package ie.setu.watch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class WatchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_watch)
-    }
 
+        Timber.plant(Timber.DebugTree())
+
+        Timber.i("Placemark Activity started..")
+    }
 }
+
