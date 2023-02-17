0a703b987036d2f60e645e1b1e7e134081ff3374
Will <20088880@mail.wit.ie>
2023-02-17 14:10:38 +0000

    null safety

--- app/src/main/java/ie/setu/watch/activities/WatchActivity.kt
+++ app/src/main/java/ie/setu/watch/activities/WatchActivity.kt
@@ -4,52 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import ie.setu.watch.databinding.ActivityWatchBinding
+import ie.setu.watch.main.MainApp
 import ie.setu.watch.models.WatchModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class WatchActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityWatchBinding
     var watch = WatchModel()
-    val watchs = ArrayList<WatchModel>()
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityWatchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        app = application as MainApp
         i("Watch Activity started...")
-
         binding.btnAdd.setOnClickListener() {
             watch.title = binding.watchTitle.text.toString()
-            if (watch.title.isNotEmpty()) {
-                i("add Button Pressed: $watch.title")
-            }
-            else {
-                Snackbar
-                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
-                    .show()
-            }
             watch.description = binding.watchDescription.text.toString()
-            if (watch.description.isNotEmpty()) {
-                i("add Button Pressed: $watch.description")
+            if (watch.title.isNotEmpty()) {
+                app.watchs.add(watch.copy())
+                i("add Button Pressed: ${watch}")
+                for (i in app.watchs.indices)
+                { i("Placemark[$i]:${this.app.watchs[i]}") }
             }
             else {
-                Snackbar
-                    .make(it,"Please Enter a description", Snackbar.LENGTH_LONG)
+                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
-
         }
-        var watch1 = WatchModel("Test Object")
-
-        watchs.add(watch1)
-
-        println("Successfully Added: " + watch1.toString())
     }
 }
+
 
 
 
--- app/src/main/java/ie/setu/watch/main/MainApp.kt
+++ app/src/main/java/ie/setu/watch/main/MainApp.kt
@@ -1,14 +1,18 @@
 package ie.setu.watch.main
 
 import android.app.Application
+import ie.setu.watch.models.WatchModel
 import timber.log.Timber
 import timber.log.Timber.i
 
 class MainApp : Application() {
 
+    val watchs = ArrayList<WatchModel>()
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        i("Watch app started")
+        i("Watch started")
     }
 }
+
