777418e0ede6bb5349853b0b870b8225c3453f38
Will <20088880@mail.wit.ie>
2023-02-18 17:19:38 +0000

    passing data

--- app/src/main/java/ie/setu/watch/models/WatchModel.kt
+++ app/src/main/java/ie/setu/watch/models/WatchModel.kt
@@ -1,3 +1,8 @@
 package ie.setu.watch.models
 
-data class WatchModel(var title: String = "", var description: String = "")
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class WatchModel(var title: String = "",
+                      var description: String = "") : Parcelable
